725238f3f0b54f67dab9af93787b8da5b9da12b2
mattmatt7660 <mdcking4749@live.com>
2026-08-23 13:05:38 -0400

    .

--- connect4.cpp
+++ connect4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 
 const int ROWS = 6;
 const int COLS = 7;
@@ -51,12 +52,82 @@ int dropPiece(Board& board, int col, char piece)
     return -1;
 }
 
+int getColInput(const Board& board, char piece)
+{
+    int col;
+    while (true)
+    {
+        std::cout << "Player " << piece << ", choose a column (1 - " << COLS << "): ";
+        if (!(std::cin >> col))
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Not a number. Try again. \n";
+            continue;
+        }
+        col -= 1;
+
+        if (!isValidMove(board, col))
+        {
+            std::cout << "Invalid move (full column/not in range). Try again.";
+            continue;
+        }
+        return col;
+    }
+}
+
+bool checkWinner(const Board& board, int row, int col, char piece)
+{
+    const int directions[4][2] = 
+    {
+        {0,1},  // horizontal
+        {1,0},  // vertical
+        {1,1},  // diagonal down right
+        {1,-1}  // diagonal down left
+    };
+
+    for (auto& direction : directions)
+    {
+        int row_diff = direction[0];
+        int col_diff = direction[1];
+        int count = 1;
+
+        // traverse forward in this direction
+        int r = row + row_diff, c = col + col_diff;
+        while (r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c] == piece)    // valid pos
+        {
+            count++;
+            r += row_diff;
+            c += col_diff;
+        }
+
+        // traverse backwards in the same direction
+        r = row - row_diff;
+        c = col - col_diff;
+        while (r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c] == piece)    // valid pos
+        {
+            count++;
+            r -= row_diff;
+            c -= col_diff;
+        }
+
+        if (count >= 4) return true;
+    }
+
+    return false;
+
+
+}
+
 
 
 int main()
 {
     std::cout << "Testing board creation: " << std::endl;
     Board board = createBoard();
+    dropPiece(board, 3, 'X');
+    dropPiece(board, 3, 'X');
+    dropPiece(board, 2, 'X');
     printBoard(board);
     return 0;
 }
